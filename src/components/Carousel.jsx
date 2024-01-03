b607cf7cc54f4f8222c44e9fc91a97cb415f0803
miawithcode <cuttontail@gmail.com>
2024-01-03 21:12:15 +0800

    feat: import data and set it as state value

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,6 +1,16 @@
+import { useState } from "react"
+import { shortList, list, longlist } from "../data"
+
 const Carousel = () => {
+  const [people, setPeople] = useState(shortList);
+
   return (
-    <div>Carousel</div>
+    <section className="slider-container">
+      {people.map((person) => {
+        const {id, image, name, title, quote} = person;
+        return <article className="slide" key={id}></article>
+      })}
+    </section>
   )
 }
 export default Carousel
